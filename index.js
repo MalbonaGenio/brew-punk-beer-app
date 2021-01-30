be7e9870b3906bb352a2d5b66bd74b9612adaca0
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-01-30 10:19:23 +0900

    added filter added logic to thte filters for alcohol and hopines so they fetch apropiate beers from the api when selected.

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 // variables
 const urlBase = "https://api.punkapi.com/v2/beers"
+const filterABV = document.getElementById("filterABV")
+let optionsABV = ""
+const filterIBU = document.getElementById("filterIBU")
+let optionsIBU = ""
 
-// Create an async function called "getBeers" that uses fetch to get our beer data from the urlBase.
-// Render each beer name inside the div with the class of beers that currently exists in the HTML file.
+//filters
+filterABV.addEventListener("change", (e) => {
+  //we want to get the vaule set in the html for that radio buttton
+  const value = e.target.value
+
+  switch (value) {
+    case "all":
+      optionsABV = ""
+      break
+    case "weak":
+      optionsABV = "abv_lt=4.6"
+      break
+    case "medium":
+      optionsABV = "abv_gt=4.5&abv_lt=7.6"
+      break
+    case "strong":
+      optionsABV = "abv_gt=7.5"
+      break
+  }
+
+  getBeers()
+})
+
+filterIBU.addEventListener("change", (e) => {
+  const value = e.target.value
+
+  switch (value) {
+    case "all":
+      optionsIBU = ""
+      break
+    case "weak":
+      optionsIBU = "ibu_lt=35"
+      break
+    case "medium":
+      optionsIBU = "ibu_gt=34&ibu_lt=75"
+      break
+    case "strong":
+      optionsIBU = "ibu_gt=74"
+      break
+  }
+
+  getBeers()
+})
 
 async function getBeers() {
-  const response = await fetch(urlBase)
+  const url = urlBase + "?" + optionsABV + "&" + optionsIBU
+  const response = await fetch(url)
   const beers = await response.json()
 
   const beersDiv = document.querySelector(".beers")
   let beerHtml = ""
-  beers.forEach(beer => {
+  beers.forEach((beer) => {
     beerHtml += `
      <div class='beer-wrapper card'>
          <div class='beer'>
@@ -26,14 +72,14 @@ async function getBeers() {
              <div class='beer__tagline'>${beer.tagline}</div>
              <div class='beer__description'>${beer.description}</div>
              <div class='beer__food-pairing'>
-                 Pair with: ${beer.food_pairing.join(', ')}
+                 Pair with: ${beer.food_pairing.join(", ")}
              </div>
          </div>
      </div>
-    `; 
- });
- 
- beersDiv.innerHTML = beerHtml;
+    `
+  })
+
+  beersDiv.innerHTML = beerHtml
 }
 
 getBeers()
