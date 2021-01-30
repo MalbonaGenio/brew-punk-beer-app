8e074e8b9eaddbbae3715a765b7795acaf1a40d8
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-01-30 10:45:45 +0900

    adde pagination added pagination to show more than the limit of 25 beers that we get from the API using the page built in the api

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // variables
-const urlBase = "https://api.punkapi.com/v2/beers"
+const urlBase = "https://api.punkapi.com/v2/beers?page="
+const pageText = document.getElementById("pageNumber")
+const prevPage = document.getElementById("prevPage")
+const nextPage = document.getElementById("nextPage")
+let page = 1
 const filterABV = document.getElementById("filterABV")
 let optionsABV = ""
 const filterIBU = document.getElementById("filterIBU")
@@ -15,16 +19,16 @@ filterABV.addEventListener("change", (e) => {
       optionsABV = ""
       break
     case "weak":
-      optionsABV = "abv_lt=4.6"
+      optionsABV = "&abv_lt=4.6"
       break
     case "medium":
-      optionsABV = "abv_gt=4.5&abv_lt=7.6"
+      optionsABV = "&abv_gt=4.5&abv_lt=7.6"
       break
     case "strong":
-      optionsABV = "abv_gt=7.5"
+      optionsABV = "&abv_gt=7.5"
       break
   }
-
+  page = 1
   getBeers()
 })
 
@@ -36,23 +40,38 @@ filterIBU.addEventListener("change", (e) => {
       optionsIBU = ""
       break
     case "weak":
-      optionsIBU = "ibu_lt=35"
+      optionsIBU = "&ibu_lt=35"
       break
     case "medium":
-      optionsIBU = "ibu_gt=34&ibu_lt=75"
+      optionsIBU = "&ibu_gt=34&ibu_lt=75"
       break
     case "strong":
-      optionsIBU = "ibu_gt=74"
+      optionsIBU = "&ibu_gt=74"
       break
   }
-
+  page = 1
   getBeers()
 })
 
 async function getBeers() {
-  const url = urlBase + "?" + optionsABV + "&" + optionsIBU
+  const url = urlBase + page + optionsABV + optionsIBU
   const response = await fetch(url)
   const beers = await response.json()
+
+  //pagination
+
+  pageText.innerHTML = page
+  if (page === 1) {
+    prevPage.disabled = true
+  } else {
+    prevPage.disabled = false
+  }
+  //if we get less than 25 beers back we don't have any more to show in a next page
+  if (beers.lentgh < 25) {
+    nextPage.disabled = true
+  } else {
+    nextPage.disabled = false
+  }
 
   const beersDiv = document.querySelector(".beers")
   let beerHtml = ""
@@ -81,5 +100,15 @@ async function getBeers() {
 
   beersDiv.innerHTML = beerHtml
 }
+
+//pagination
+prevPage.addEventListener("click", () => {
+  page--
+  getBeers()
+})
+nextPage.addEventListener("click", () => {
+  page++
+  getBeers()
+})
 
 getBeers()
