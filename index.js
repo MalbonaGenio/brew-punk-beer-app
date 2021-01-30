207c4ffdd219ae23f4106ab62cbf8815abbab4d3
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-01-30 09:12:10 +0900

    updated the beersDiv Added some more elemets to the beers div to create the cards with image and additional info of the beers

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 // variables
-const urlBase = "https://api.punkapi.com/v2/beers";
+const urlBase = "https://api.punkapi.com/v2/beers"
 
 // Create an async function called "getBeers" that uses fetch to get our beer data from the urlBase.
 // Render each beer name inside the div with the class of beers that currently exists in the HTML file.
 
 async function getBeers() {
-  const response = await fetch(urlBase);
-  const beers = await response.json();
+  const response = await fetch(urlBase)
+  const beers = await response.json()
 
-  const beersDiv = document.querySelector(".beers");
-  let beerHtml = "";
+  const beersDiv = document.querySelector(".beers")
+  let beerHtml = ""
   beers.forEach((beer) => {
     beerHtml += `
-        <h3>${beer.name}</h3>
-        `;
-  });
+     <div class='beer-wrapper card'>
+         <div class='beer'>
+             <img class='beer__img' src="${beer.image_url}">
+             <h3>${beer.name}</h3>
+             <span class='beer__info'>
+                 <span>ABV: ${beer.abv}%</span>
+                 <span>IBU: ${beer.ibu}</span>
+             </span>
+         </div>
+     </div>
+    `
+  })
 
-  beersDiv.innerHTML = beerHtml;
+  beersDiv.innerHTML = beerHtml
 }
 
-getBeers();
+getBeers()
